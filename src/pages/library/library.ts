3a57d79d10480b3e33d2263824a346fc1e78c64d
SHARMA Abhishek <abhishek.sharma3@soprasteria.com>
2018-09-25 17:55:51 +0530

    Quotes data added

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Quote } from '../../data/quote.interface';
+import { Icu } from '@angular/compiler/src/i18n/i18n_ast';
+import quotes from '../../data/quotes';
 
 /**
  * Generated class for the LibraryPage page.
@@ -15,7 +18,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LibraryPage {
 
+  quotesCollection : { category: string , quotes : Quote[] , icon : string }[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.quotesCollection = quotes;
   }
 
   ionViewDidLoad() {
